48488f9d4dedd318a550966ea8b831bcf3faee51
Asawer-Javed <152972047+Asawer-Javed@users.noreply.github.com>
2024-06-26 13:22:20 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,224 @@
+let questions = [
+    {
+        numb : 1,
+        question : "Which Pakistani physicist was awarded the Nobel Prize in Physics in 1979 for his work on the unified theory of weak and electromagnetic interactions?",
+        answer : "b) Abdus Salam",
+        options : [
+            "a) Abdul Qadeer Khan",
+            "b) Abdus Salam",
+            "c) Pervez Hoodbhoy",
+            "d) Atta-ur-Rahman"
+        ]
+    },
+    {
+        numb : 2,
+        question : "What is the national flower of Pakistan?",
+        answer : "d) Jasmine",
+        options : [
+            "a) Rose",
+            "b) Lily",
+            "c) Sunflower",
+            "d) Jasmine"
+        ]
+    },
+    {
+        numb : 3,
+        question : "The Taj Mahal, one of the most famous landmarks in the world, was built by which Mughal emperor?",
+        answer : "c) Shah Jahan",
+        options : [
+            "a) Akbar",
+            "b) Jahangir",
+            "c) Shah Jahan",
+            "d) Aurangzeb"
+        ]
+    },
+    {
+        numb : 4,
+        question : "Which country has won the most Olympic gold medals in the history of the Summer Olympics?",
+        answer: "a) United States",
+        options : [
+            "a) United States",
+            "b) China",
+            "c) Russia",
+            "d) Great Britain"
+        ]
+    },
+    {
+        numb : 5,
+        question : "The ancient city of Mohenjo-Daro, belonging to the Indus Valley Civilization, is located in which modern-day country?",
+        answer : "d) Pakistan",
+        options : [
+            "a) India",
+            "b) Afghanistan",
+            "c) Bangladesh",
+            "d) Pakistan"
+
+        ]
+    },
+    {
+        numb : 6,
+        question : "In which year did Pakistan win its first Cricket World Cup?",
+        answer: "c) 1992",
+        options : [
+            "a) 1983",
+            "b) 1996",
+            "c) 1992",
+            "d) 1975"
+        ]
+    },
+    {
+        numb : 7,
+        question : "Who is often referred to as the 'Sultan of Swing' in cricket and is considered one of the greatest fast bowlers of all time from Pakistan?",
+        answer : "b) Wasim Akram",
+        options : [
+            "a) Imran Khan",
+            "b) Wasim Akram",
+            "c) Waqar Younis",
+            "d) Shoaib Akhtar"
+        ]
+    },
+    {
+        numb : 8,
+        question : " Which Pakistani musician is known for his fusion of classical music with modern sounds and is often referred to as the 'Rockstar Ustad'?",
+        answer : "d) Ali Zafar",
+        options : [
+            "a) Nusrat Fateh Ali Khan",
+            "b) Atif Aslam",
+            "c) Rahat Fateh Ali Khan",
+            "d) Ali Zafar"
+        ]
+    },
+    {
+        numb : 9,
+        question : "Who was the first woman to win a Nobel Prize?",
+        answer : "a) Marie Curie",
+        options : [
+            "a) Marie Curie",
+            "b) Mother Teresa",
+            "c) Wangari Maathai",
+            "d) Malala Yousafzai"
+        ]
+    },
+    {
+        numb : 10,
+        question : "What is the chemical symbol for the element gold?",
+        answer : "b) Au",
+        options : [
+            "a) Ag",
+            "b) Au",
+            "c) Gd",
+            "d) Pt"
+        ]
+    },
+    {
+        numb : 11,
+        question : "Which scientist is known for his theory of relativity, expressed by the equation E=mc^2?",
+        answer : "b) Albert Einstein",
+        options : [
+            "a) Isaac Newton",
+            "b) Albert Einstein",
+            "c) Stephen Hawking",
+            "d) Nikola Tesla"
+        ]
+    },
+    {
+        numb : 12,
+        question : "Which planet is known as the 'Red Planet'?",
+        answer : "c) Mars",
+        options : [
+            "a) Venus",
+            "b) Jupiter",
+            "c) Mars",
+            "d) Saturn"
+        ]
+    },
+    {
+        numb : 13,
+        question : "Who was the first human to journey into outer space?",
+        answer : "a) Yuri Gagarin",
+        options : [
+            "a) Yuri Gagarin",
+            "b) Neil Armstrong",
+            "c) Buzz Aldrin",
+            "d) John Glenn"
+        ]
+    },
+    {
+        numb : 14,
+        question : "What is the chemical symbol for the element oxygen?",
+        answer : "d) O",
+        options :[
+            "a) Om",
+            "b) Ox",
+            "c) Oc",
+            "d) O"
+        ]
+    },
+    {
+        numb : 15,
+        question : "In which year did the FIFA World Cup tournament begin?",
+        answer : "b) 1930",
+        options : [
+            "a) 1926",
+            "b) 1930",
+            "c) 1934",
+            "d) 1950"
+        ]
+    },
+    {
+        numb : 16,
+        question : "Who is known as the father of modern physics for his formulation of the laws of motion and universal gravitation?",
+        answer : "a) Isaac Newton",
+        options : [
+            "a) Isaac Newton",
+            "b) Galileo Galilei",
+            "c) Albert Einstein",
+            "d) Johannes Kepler"
+        ]
+    },
+    {
+        numb : 17,
+        question : "What is the tallest mountain in the world?",
+        answer: "c) Mount Everest",
+        options : [
+            "a) K2",
+            "b) Mount Kilimanjaro",
+            "c) Mount Everest",
+            "d) Mount McKinley (Denali)"
+        ]
+    },
+    {
+        numb : 18,
+        question : "Who is known for formulating the theory of evolution by natural selection?",
+        answer: "b) Charles Darwin",
+        options : [
+            "a) Gregor Mendel",
+            "b) Charles Darwin",
+            "c) Louis Pasteur",
+            "d) Alexander Fleming"
+        ]
+    },
+    {
+        numb : 19,
+        question : "Who holds the record for the fastest century in international cricket?",
+        answer: "a) AB de Villiers",
+        options :[
+            "a) AB de Villiers",
+            "b) Shahid Afridi",
+            "c) Chris Gayle",
+            "d) Virat Kohli"
+        
+        ]
+    },
+    {
+        numb : 20,
+        question : "Who is the author of the Harry Potter book series?",
+        answer: "b) J.K. Rowling",
+        options : [
+            "a) J.R.R. Tolkien",
+            "b) J.K. Rowling",
+            "c) George R.R. Martin",
+            "d) Stephenie Meyer"
+        ]
+    }
+];
